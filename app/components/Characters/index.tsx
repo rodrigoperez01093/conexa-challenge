79e290f5db79256929504291517157c74357b130
rodrigoperez01093 <rodrigoperez0109@gmail.com>
2025-03-22 19:21:02 -0300

    initial repo

---
+++ app/components/Characters/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CharactersView = () => {
+  return (
+    <div className='w-full h-full'>
+      <h1>First steps</h1>
+    </div>
+  )
+}
+
+export default CharactersView;
